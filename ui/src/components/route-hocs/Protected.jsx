88fc4a6e396b7202b249623e24a2937ea0ad6a15
Halefom Alemu <hannelalex12@gmail.com>
2024-10-20 22:36:11 +0300

    some more changes

--- ui/src/components/route-hocs/Protected.jsx
+++ ui/src/components/route-hocs/Protected.jsx
@@ -1,12 +1,9 @@
 import { useSelector } from 'react-redux'
 import { Navigate } from 'react-router-dom'
-export default function Protected({ component, roles = [] }) {
-  // roles: roles allowed to see this page
+export default function Protected({ component }) {
   const auth = useSelector((state) => state.auth)
 
   if (!auth.signedIn) return <Navigate to='/login' replace={true} />
-
-  // if (!roles.includes(auth.user.role)) return <Navigate to="/" replace={true} />
 
   return component
 }
