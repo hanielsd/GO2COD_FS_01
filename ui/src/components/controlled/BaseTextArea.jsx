8c557b90684f57ca21252df6153671a040328965
Halefom Alemu <hannelalex12@gmail.com>
2024-10-21 01:17:11 +0300

    added error props in base text area component

--- ui/src/components/controlled/BaseTextArea.jsx
+++ ui/src/components/controlled/BaseTextArea.jsx
@@ -1,18 +1,22 @@
 import { forwardRef } from 'react'
 
 const BaseTextArea = forwardRef(
-  ({ className, label, placeholder = '', ...rest }, ref) => {
+  ({ className, label, placeholder = '', error = '', ...rest }, ref) => {
     return (
       <div className={'form-control ' + className}>
         <label className='label'>
           <span className='label-text uppercase'>{label}</span>
         </label>
         <textarea
-          className='textarea textarea-bordered'
+          className={
+            'textarea textarea-bordered ' + (error ? 'textarea-error' : '')
+          }
           placeholder={placeholder}
           ref={ref}
           {...rest}
         ></textarea>
+
+        {error && <div className='text-red-600 pt-1 text-sm'>{error}</div>}
       </div>
     )
   },
