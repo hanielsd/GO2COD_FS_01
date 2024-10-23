90b576391d9de9803a8656a9d3a484713adf57c0
Halefom Alemu <hannelalex12@gmail.com>
2024-10-23 15:27:39 +0300

    added avatar component and used it in post list for author

--- ui/src/components/cards/Post.jsx
+++ ui/src/components/cards/Post.jsx
@@ -1,8 +1,12 @@
+import Avatar from '../collection/Avatar'
+
 export default function Post({ post }) {
+  const authorName = post.author.firstName + ' ' + post.author.lastName
   return (
     <div className='p-2 cursor-pointer group'>
       <div className='flex space-x-2 items-center'>
-        <div className='text-sm'>{post.author.firstName}</div>
+        <Avatar name={authorName} size={22} />
+        <div className='text-sm'>{authorName}</div>
       </div>
       <div className='text-xl font-medium group-hover:text-primary'>
         {post.title}
