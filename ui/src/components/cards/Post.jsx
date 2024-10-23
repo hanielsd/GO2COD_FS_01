fa65d0db113f4b1d39777a4b5d60261deb932452
Halefom Alemu <hannelalex12@gmail.com>
2024-10-23 15:57:27 +0300

    added a 'you' badge if the author is you

--- ui/src/components/cards/Post.jsx
+++ ui/src/components/cards/Post.jsx
@@ -1,13 +1,22 @@
+import { useSelector } from 'react-redux'
 import { timeAgo } from '../../../utils'
 import Avatar from '../collection/Avatar'
 
 export default function Post({ post }) {
   const authorName = post.author.firstName + ' ' + post.author.lastName
+  const { user, signedIn } = useSelector((state) => state.auth)
+  const isItYou = signedIn && user.id === post.author._id
+
   return (
     <div className='p-2 cursor-pointer group space-y-1'>
       <div className='flex space-x-2 items-center'>
         <Avatar name={authorName} size={22} />
         <div className='text-[13px]'>{authorName}</div>
+        {isItYou && (
+          <div className='px-2 bg-gray-300 text-gray-600 text-xs rounded-full'>
+            You
+          </div>
+        )}
       </div>
       <div>
         <div className='text-xl font-medium group-hover:text-primary'>
